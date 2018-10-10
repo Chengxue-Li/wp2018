53c31799d7a0bdf12ba4d5ab081e38dd80e17806
Chengxue-Li <930leo@gmail.com>
2018-10-10 22:38:57 +0800

    HW3 complete

--- HW3/main.js
+++ HW3/main.js
@@ -1,18 +1,77 @@
-
 $(document).ready(function() {
-    $("#ajax_form button").click(function(event) {
-        event.preventDefault();//取消reload
-        
-        $.ajax({
-            method: "get",
-            url: "./ajax_data",
-            data: {
-                fname: $("#ajax_form input[name='fname']").val(),
-                lname: $("#ajax_form input[name='lname']").val()
-            },
-            success: function(data) {
-                $("#ajax_content").text(data);
-            }
-        })
-    })
-})
+    bindListButton();
+    bindSearchButton();
+    bindAddButton();
+    bindDeleteButton();
+});
+function bindListButton() {
+  $("#list_form button").click(function(event) {
+    $("#list_text").text("loading...");
+    $.ajax({
+      method: "get",
+      url: "/ajax",
+      data: {
+        type: "list"
+      },
+      success: function(data) {
+        var str = "";
+        var text = $("#list_text");
+        text.text("");
+        $.each(data, function(index, value) {
+          str += ("\"" + index + "\": \"" + value + "\"\n");
+        });
+        text.text(str);
+        text.html(text.html().replace(/\n/g, '<br/>'));
+      }
+    });
+  });
+}
+function bindSearchButton() {
+  $("#search_form button").click(function(event) {
+    $("#search_text").text("loading...");
+    $.ajax({
+      method: "get",
+      url: "/ajax",
+      data: {
+        type: "search",
+        id_num: $("#search_form input[name='student_id']").val()
+      },
+      success: function(data) {
+        $("#search_text").text(data);
+      }
+    });
+  });
+}
+function bindAddButton() {
+  $("#add_form button").click(function(event) {
+    $("#add_text").text("adding...");
+    $.ajax({
+      method: "get",
+      url: "/ajax",
+      data: {
+        type: "add",
+        id_num: $("#add_form input[name='student_id']").val(),
+        name: $("#add_form input[name='student_name']").val()
+      },
+      success: function(data) {
+        $("#add_text").text(data);
+      }
+    });
+  });
+}
+function bindDeleteButton() {
+  $("#delete_form button").click(function(event) {
+    $("#delete_text").text("deleting");
+    $.ajax({
+      method: "get",
+      url: "/ajax",
+      data: {
+        type: "delete",
+        id_num: $("#delete_form input[name='student_id']").val()
+      },
+      success: function(data) {
+        $("#delete_text").text(data);
+      }
+    });
+  });
+}
